a7f69af562793ca464e8101e6fca5b5037f129fe
isaac-asdf <97187398+isaac-asdf@users.noreply.github.com>
2023-09-19 21:09:08 -0500

    build working again

--- src/lib/components/TempGraph.svelte
+++ src/lib/components/TempGraph.svelte
@@ -3,11 +3,11 @@
 	import Axis from './Axis.svelte';
 	// @ts-ignore
 	import * as d3 from 'd3';
+	import { onMount } from 'svelte';
 
 	type WeatherPoint = { temp: number; time: Date };
 
 	let data: WeatherPoint[] = [];
-
 	$weatherStore.forEach((event) => {
 		const time = new Date(event.created_at! * 1000);
 		const celsius = parseFloat(event.content);
@@ -15,7 +15,11 @@
 		data.push({ temp, time });
 	});
 
-	let ctx;
+	onMount(() => {
+		if (timeRange != null) {
+			data.filter(({ time }) => time > timeRange!.low && time < timeRange!.high);
+		}
+	});
 
 	function celsiusToF(celsius: number): number {
 		return (celsius * 9) / 5 + 32;
@@ -24,26 +28,9 @@
 	export let width = 640;
 	export let height = 400;
 	export let margin = 30;
-
-	let minTime: Date | null = null;
-	let maxTime: Date | null = null;
-
-	let inputTimeChange = () => {
-		if (minTime && maxTime) {
-			if (minTime > maxTime) {
-				const temp = minTime;
-				minTime = maxTime;
-				maxTime = temp;
-			}
-		}
-	};
+	export let timeRange: { low: Date; high: Date } | null = null;
 
 	$: timeExtent = d3.extent(data, (d: WeatherPoint) => d.time);
-	if (minTime && maxTime) {
-		console.log('time changed');
-		timeExtent[0] = minTime;
-		timeExtent[1] = maxTime;
-	}
 	$: x = d3.scaleTime(timeExtent, [margin, width - margin]);
 	$: y = d3.scaleLinear(d3.extent(data, (d: WeatherPoint) => d.temp).reverse(), [
 		margin * 2,
@@ -56,40 +43,25 @@
 		.y((d: WeatherPoint) => y(d.temp));
 </script>
 
-<div class="border-b-2 hover:border-2 max-w-max">
-	<label for="minDate">Min. Date:</label>
-	<input
-		class="text-slate-400"
-		id="minDate"
-		type="datetime-local"
-		bind:value={minTime}
-		on:change={inputTimeChange}
-	/>
-</div>
-<div class="border-b-2 hover:border-2 max-w-max">
-	<label for="maxDate">Max Date:</label>
-	<input
-		class="text-slate-400"
-		id="maxDate"
-		type="datetime-local"
-		bind:value={maxTime}
-		on:change={inputTimeChange}
-	/>
-</div>
-
-<svg {width} {height}>
-	<path fill="none" stroke="currentColor" stroke-width="1.5" d={line(data)} />
-	<Axis {width} {height} {margin} scale={x} position="bottom" />
-	<Axis {width} {height} {margin} scale={y} position="left" />
-	<text y={height} x={width / 2}>Time</text>
-	{#if data.length > 0}
-		<text y={margin} x={margin / 2}
-			>Temperature in &degF from {timeExtent[0].toLocaleDateString()} to {timeExtent[1].toLocaleDateString()}</text
-		>
+<div>
+	{#if timeRange}
+		<text>Using manually selected time range...</text>
 	{/if}
-	<g fill="white" stroke="currentColor" stroke-width="1.5">
-		{#each data as d, i}
-			<circle cx={x(d.time)} cy={y(d.temp)} r="1" />
-		{/each}
-	</g>
-</svg>
+	<!-- <svg class="width:100% max-w-screen-sm" viewBox="0 0 {width + margin * 2} {height + margin * 2}"> -->
+	<svg viewBox="0 0 {width + margin * 2} {height + margin * 2}">
+		<path fill="none" stroke="currentColor" stroke-width="1.5" d={line(data)} />
+		<Axis {width} {height} {margin} scale={x} position="bottom" />
+		<Axis {width} {height} {margin} scale={y} position="left" />
+		<text y={height} x={width / 2}>Time</text>
+		{#if data.length > 0}
+			<text y={margin} x={margin / 2}
+				>Temperature in &degF from {timeExtent[0].toLocaleDateString()} to {timeExtent[1].toLocaleDateString()}</text
+			>
+		{/if}
+		<g fill="white" stroke="currentColor" stroke-width="1.5">
+			{#each data as d, i}
+				<circle cx={x(d.time)} cy={y(d.temp)} r="1" />
+			{/each}
+		</g>
+	</svg>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,23 @@
 	// Will return all found events
 	const events = $ndk.storeSubscribe(filter, { closeOnEose: false });
 
+	let timeRangeInput: { low: Date | null; high: Date | null } = { low: null, high: null };
+	let timeRange: { low: Date; high: Date } = { low: new Date(), high: new Date() };
+
+	let inputTimeChange = () => {
+		if (timeRangeInput.low && timeRangeInput.high) {
+			if (timeRangeInput.high > timeRangeInput.low) {
+				timeRange.low = timeRangeInput.low;
+				timeRange.high = timeRangeInput.high;
+				useTimeRange = true;
+				return;
+			}
+		}
+		useTimeRange = false;
+	};
+
+	let useTimeRange = false;
+
 	let showRelays = false;
 </script>
 
@@ -20,13 +37,38 @@
 </svelte:head>
 
 <div class="margin:40px">
-	<h1>Nostr Weather Report</h1>
-	<button class="bg-slate-200 hover:bg-slate-300" on:click={() => (showRelays = !showRelays)}
-		>Click for relays</button
-	>
-	<TempGraph />
+	<h1 class="font-bold text-2xl underline">Nostr Weather Report</h1>
+	<div class="border-b-2 hover:border-2 max-w-max">
+		<label for="minDate">Min. Date:</label>
+		<input
+			class="text-slate-400"
+			id="minDate"
+			type="datetime-local"
+			bind:value={timeRangeInput.low}
+			on:change={inputTimeChange}
+		/>
+	</div>
+	<div class="border-b-2 hover:border-2 max-w-max">
+		<label for="maxDate">Max Date:</label>
+		<input
+			class="text-slate-400"
+			id="maxDate"
+			type="datetime-local"
+			bind:value={timeRangeInput.high}
+			on:change={inputTimeChange}
+		/>
+	</div>
+	{#if useTimeRange}
+		Using time Range
+		<TempGraph {timeRange} />
+	{:else}
+		<TempGraph />
+	{/if}
 
 	{#if showRelays}
+		<button class="bg-slate-200 hover:bg-slate-300" on:click={() => (showRelays = !showRelays)}
+			>Click for relays</button
+		>
 		<RelayList ndk={$ndk} />
 	{/if}
 </div>
